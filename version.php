796ad1beae0e43775d3bea99b546240ab66a95b4
valery <valery.fremaux@gmail.com>
2017-11-15 11:40:00 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2017033101; // Plugin version.
-$plugin->requires = 2016112900; // Required Moodle version.
+$plugin->requires = 2017050500; // Required Moodle version.
 $plugin->component = 'format_page';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.2.0 (Build 2017033101)';
+$plugin->release = '3.3.0 (Build 2017033101)';
 $plugin->dependencies = array('block_page_module' => 2016100500);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0007';
+$plugin->codeincrement = '3.3.0007';
